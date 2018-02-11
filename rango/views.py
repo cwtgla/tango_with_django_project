45d504a252a38edf6a9f1cecd98cf49bfc558159
Craig Thomson <2282467t@student.gla.ac.uk>
2018-02-11 21:03:27 +0000

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -9,30 +9,39 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
 
-def visitor_cookie_handler(request, response):
+#helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+#updated function definition
+def visitor_cookie_handler(request):
 	#get the number of visits tot he site
 	#use cookies.get() function to obtain the visits cookie
 	#if it exists the value returned is casted to an integer
 	#if the cookie doesnt exist the default value of 1 is used
-	visits = int(request.COOKIES.get('visits','1'))
+	visits = int(get_server_side_cookie(request,'visits','1'))
 
-	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
 	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
 	#if its been more than a day since the last visit
 	if(datetime.now() - last_visit_time).days > 0:
 		visits = visits + 1
 		#update the last visit cookie now that we have updated the count
-		response.set_cookie('last_visit', str(datetime.now()))
+		request.session['last_visit'] = str(datetime.now())
 	else:
 		visits = 1
 		#set the last visit cookie
-		response.set_cookie('last_visit', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
 
 	#uodate set the visits cookie
-	response.set_cookie('visits', visits)
+	request.session['visits'] = visits
 
 def index(request):
+	request.session.set_test_cookie()
 	# Query the database for a list of ALL categories currently stored.
 	# Order the categories by no. likes in descending order.
 	# Retrieve the top 5 only - or all if less than 5.
@@ -42,9 +51,10 @@ def index(request):
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories':category_list, 'pages':page_list}
 
-	response = render(request, 'rango/index.html', context_dict)
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 
-	visitor_cookie_handler(request, response)
+	response = render(request, 'rango/index.html', context=context_dict)
 
 	return response
 
@@ -113,7 +123,12 @@ def about(request):
 	if request.session.test_cookie_worked():
 		print("TEST COOKIE WORKED!")
 		request.session.delete_test_cookie()
-	return render(request, 'rango/about.html')
+
+	visitor_cookie_handler(request)
+	context_dict = {}
+	context_dict['visits'] = request.session['visits']
+
+	return render(request, 'rango/about.html', context=context_dict)
 
 @login_required
 def restricted(request):
