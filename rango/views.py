4c8bdc866226bfa67510bebbef1338d27576b678
Craig Thomson <2282467t@student.gla.ac.uk>
2018-02-11 20:38:08 +0000

    bit of ch10

--- rango/views.py
+++ rango/views.py
@@ -7,8 +7,33 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
+def visitor_cookie_handler(request, response):
+    #get the number of visits tot he site
+    #use cookies.get() function to obtain the visits cookie
+    #if it exists the value returned is casted to an integer
+    #if the cookie doesnt exist the default value of 1 is used
+    visits = int(request.COOKIES.get('visits','1'))
+
+    last_visits_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    #if its been more than a day since the last visit
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        #update the last visit cookie now that we have updated the count
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        visits = 1
+        #set the last visit cookie
+        response.set_cookie('last_visit', last_visits_cookie)
+
+    #uodate set the visits cookie
+    response.set_cookie('visits', visits)
 
 def index(request):
+	request.session.set_test_cookie()
 	# Query the database for a list of ALL categories currently stored.
 	# Order the categories by no. likes in descending order.
 	# Retrieve the top 5 only - or all if less than 5.
@@ -84,6 +109,9 @@ def register(request):
 
 
 def about(request):
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
 	return render(request, 'rango/about.html')
 
 @login_required
